a8383e404f0aad785bfda0f4ee4562f2c0394a39
Yiwei Liu <2814824250@qq.com>
2024-11-28 11:11:51 +0000

    chores: script

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/_not-found', // 捕获 /_not-found 路径
+        destination: '/', // 将其重定向到首页（或其他页面）
+        permanent: false, // 使用临时重定向，非永久
+      },
+    ];
+  },
   async rewrites() {
     return [
       {
